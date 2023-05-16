f7e808df3feade99d2b8d4582b4bccca75a27f12
AlexTimoncini <alextimonci@gmail.com>
2023-05-16 17:21:09 +0200

    add let to variable i inside the for cicle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const numberListDOM = document.querySelector('.ivy_container ul');
 
-for (i = 1; i <= 100; i++){
-let numberItemDOM = document.createElement('li');
+for (let i = 1; i <= 100; i++){
+    let numberItemDOM = document.createElement('li');
     if (i % 3 === 0 && i % 5 !== 0){
-        numberItemDOM.classList.add('fizz_number')
+        numberItemDOM = document.createElement('li');
+        numberItemDOM.classList.add('fizz_number');
         numberItemDOM.append('Fizz');
     } else if (i % 5 === 0 && i % 3 !== 0) {
         numberItemDOM.classList.add('buzz_number')
